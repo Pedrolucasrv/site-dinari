ab070f224f6b5e24b3dd675d70da45426a7d19f7
Pedro Lucas <pedro.lucasrv@outlook.com>
2022-12-21 15:47:41 -0300

    first commit

---
+++ animations.js
@@ -0,0 +1,54 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+ gsap
+    .timeline({
+      scrollTrigger: {
+        trigger:'.about-us',
+        start: "-100px bottom",
+        end: "+=50",
+        scrub: 3,
+        markers: false,
+      }
+    })
+    .from('.about-us', {
+        y: 400
+    })
+    .from('.overlayed-about-us', {
+        y: 150
+    },'<')
+  ;
+
+
+gsap
+.timeline({
+    scrollTrigger: {
+
+    trigger: '.about-us-timeline',
+    start: 'top top',
+    scrub: 5,
+    endTrigger: ".end-scp",
+    end: "+=500",
+    pinSpacing: true,
+    pin:true,
+    markers: true
+    }
+})
+.from('.dinari-animated-logo', {
+    x: 50,
+    transform: 'rotate(13deg)'
+})  .to('.timeline', {
+    '--timeline-before': '295px',
+}, '<' )
+.to('.timeline2', {
+  '--timeline-opacity': '1',
+}, '>' ).from('.timeline2  .h2', {
+  x: 100,
+  opacity:0
+}, '<' )
+.from('.timeline2  .display-6', {
+  y: 100,
+  opacity:0
+}, '<' )
+
+
